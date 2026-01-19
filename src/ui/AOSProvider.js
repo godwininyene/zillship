0f9b22f1428f839e0e31509ab675236e2a3fba37
godwininyene <godwinhigh2@gmail.com>
2026-01-19 20:30:04 +0100

    WIP: Import AOS Dynamically

--- src/ui/AOSProvider.js
+++ src/ui/AOSProvider.js
@@ -1,19 +1,51 @@
-
 "use client"; 
 
 import { useEffect } from 'react';
-import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 export default function AOSProvider({ children }) {
   useEffect(() => {
-    AOS.init({
-      duration: 800, // Customize your settings
-      once: false, // Animation repeats every time you scroll up/down
-    });
-    // Optional: Refresh AOS when content changes dynamically
-    // AOS.refresh(); 
+    let aosInstance;
+
+    (async () => {
+      const AOS = (await import('aos')).default;
+
+      AOS.init({
+        duration: 800,
+        once: false,
+      });
+
+      aosInstance = AOS;
+    })();
+
+    return () => {
+      // optional cleanup
+      if (aosInstance?.refreshHard) {
+        aosInstance.refreshHard();
+      }
+    };
   }, []);
 
   return <>{children}</>;
 }
+
+
+
+// "use client"; 
+
+// import { useEffect } from 'react';
+// import AOS from 'aos';
+// import 'aos/dist/aos.css';
+
+// export default function AOSProvider({ children }) {
+//   useEffect(() => {
+//     AOS.init({
+//       duration: 800, // Customize your settings
+//       once: false, // Animation repeats every time you scroll up/down
+//     });
+//     // Optional: Refresh AOS when content changes dynamically
+//     // AOS.refresh(); 
+//   }, []);
+
+//   return <>{children}</>;
+// }
